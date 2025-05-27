69a29b506a9dc4d831de86139365edba9d7ac44b
Gwen <oddball@oddball.tf>
2025-05-27 11:05:37 -0700

    Combine files + add Linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 import gulp from 'gulp';
 import zip from 'gulp-zip';
+import jsonlint from 'gulp-jsonlint';
 
 export default () => (
   gulp.src('src/*')
+      .pipe(jsonlint())
+      .pipe(jsonlint.reporter('verbose'))
       .pipe(zip('Gwen\'s Smol or Tall for Females.pmp'))
       .pipe(gulp.dest('./dist/'))
 )
